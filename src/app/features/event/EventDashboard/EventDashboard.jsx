0c3adcca4ecdcf2a7c8efb7e8807d8c2ddf2e809
hasbynatawijaya <mhasbyn@gmail.com>
2018-11-01 07:03:39 +0700

    Input Static Data

--- src/app/features/event/EventDashboard/EventDashboard.jsx
+++ src/app/features/event/EventDashboard/EventDashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react' //type rcc the press tab shortcut
 import {Grid, Button} from 'semantic-ui-react'
 import EventList from '../EventList/EventList'
 import EventForm from '../EventForm/EventForm'
+import cuid from 'cuid'
 
 const eventsDashBoard = [
   {
@@ -74,6 +75,16 @@ class EventDashboard extends Component {
     })
   }
 
+  handleCreateEvent = (newEvent) => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = '/assets/user.png';
+    const updatedEvents = [...this.state.events, newEvent];
+    this.setState({
+      events: updatedEvents,
+      isOpen: false
+    })
+  }
+
   render() {
     return (
      <Grid>
@@ -83,7 +94,7 @@ class EventDashboard extends Component {
         <Grid.Column width={6}>
           <Button onClick={this.handleFormOpen} positive content='Create Event'/>
           {this.state.isOpen &&
-          <EventForm handleCancel={this.handleCancel}/>}
+          <EventForm createEvent={this.handleCreateEvent} handleCancel={this.handleCancel}/>}
         </Grid.Column>
      </Grid>
     )
--- src/app/features/event/EventList/EventList.jsx
+++ src/app/features/event/EventList/EventList.jsx
@@ -6,7 +6,9 @@ class EventList extends Component {
     const {events} = this.props;
     return (
       <div>
-        <h1>Event List</h1>
+        <h1>
+          Event List
+        </h1>
         {events.map((event) =>(
           <EventListItem key={event.id} event={event}/>
         ))}
