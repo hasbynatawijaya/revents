4c0e63fd2cf7cc73bf77aaa3fd6faef43fb27e40
hasbynatawijaya <mhasbyn@gmail.com>
2018-11-04 16:15:10 +0700

    Mastering Store and Get the detailed Event Pages

--- src/app/features/testarea/testReducer.js
+++ src/app/features/testarea/testReducer.js
@@ -13,17 +13,6 @@ export const decrementCounter = (state, payload) => {
     return {...state, data: state.data - 1};
 }
 
-// const testReducer = (state = initialState, action) => {
-//   switch (action.type) {
-//     case INCREMENT_COUNTER:
-//       return { ...state, data: state.data + 1 };
-//     case DECREMENT_COUNTER:
-//       return { ...state, data: state.data - 1 };
-//     default:
-//       return state;
-//   }
-// };
-
 export default createReducer(initialState, {
     [INCREMENT_COUNTER]: incrementCounter,
     [DECREMENT_COUNTER]: decrementCounter
